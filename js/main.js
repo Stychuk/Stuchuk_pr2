fd914862ab8f3cc356de2ab97b56468c32346bcb
Stuchuck <malinka27032004@gmail.com>
2022-09-16 14:21:42 +0300

    WOW.min.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-const cartButton = document.querySelector('#cart-button');
-const 
-cartButton.addEventListener('click', function(event) {});
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+new Window.
